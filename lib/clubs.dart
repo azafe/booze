1466fc72ce99b32bcb7a57c3a25337108bfaddb6
Alvaro Zafe <alvarozafe@gmail.com>
2021-11-30 16:20:24 -0300

    List View 2.0
    4 bares agregados y BottomNavigationBar

--- lib/clubs.dart
+++ lib/clubs.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class Clubs extends StatelessWidget {
@@ -13,52 +15,188 @@ class Clubs extends StatelessWidget {
           title: Text('BoozApp'),
         ),
         body: ListView(
-          padding: EdgeInsets.only(right: 20, left: 20, top: 5),
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(20), //(right: 20, left: 20, top: 5),
           children: <Widget>[
-            ListTile(
-              title: Text("Patagonia Sheraton"),
-              subtitle: Text("Av. Soldati 400"),
-              leading: Icon(Icons.ac_unit),
-              trailing: Icon(Icons.account_box),
-              tileColor: Colors.blue,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)),
-            ),
-            Container(
-              margin: EdgeInsets.all(8),
-              child: Card(
-                color: Colors.grey,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.stretch,
-                  children: <Widget>[
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topLeft: Radius.circular(8),
-                        topRight: Radius.circular(8),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            Card(
-              child: Image.asset(
-                'assets/images/1.png',
-                fit: BoxFit.fitHeight,
-              ),
-              color: Colors.orange,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30)),
-            ),
-            Card(
-              color: Colors.green,
-            ),
-            Card(
-              color: Colors.orange,
-            ),
+            patagoniaSheraton(),
+            barIrlanda(),
+            pollock(),
+            patagoniaHilton(),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.qr_code_sharp), label: 'Escanear'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.business), label: 'Bussines'),
           ],
         ));
   }
+}
+
+Widget patagoniaSheraton() {
+  return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: Container(
+          height: 150,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: DecorationImage(
+                  image: AssetImage('assets/images/1.png'), fit: BoxFit.cover)),
+          child: Stack(
+            children: [
+              Positioned(
+                  top: 120,
+                  left: 10,
+                  child: Text(
+                    "Patagonia Sheraton",
+                    style: TextStyle(
+                      shadows: [
+                        Shadow(
+                            blurRadius: 10,
+                            color: Colors.black,
+                            offset: Offset(1, 4))
+                      ],
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  )),
+            ],
+          )));
+}
+
+Widget barIrlanda() {
+  return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: Container(
+        height: 150,
+        child: Stack(children: [
+          Positioned(
+              top: 120,
+              left: 10,
+              child: Text(
+                "Bar Irlanda",
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                        blurRadius: 10,
+                        color: Colors.black,
+                        offset: Offset(1, 4))
+                  ],
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Container(
+            width: double.infinity,
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: Colors.black12),
+          )
+        ]),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+              image: AssetImage('assets/images/bar_irlanda.jpg'),
+              fit: BoxFit.cover),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black,
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: Offset(0, 1))
+          ],
+        ),
+      ));
+}
+
+Widget pollock() {
+  return Container(
+    height: 150,
+    child: Stack(children: [
+      Positioned(
+          top: 120,
+          left: 10,
+          child: Text(
+            "Pollock",
+            style: TextStyle(
+              shadows: [
+                Shadow(
+                    blurRadius: 10, color: Colors.black, offset: Offset(1, 4))
+              ],
+              color: Colors.white54,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          )),
+      Container(
+        width: double.infinity,
+        height: 150,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20), color: Colors.black12),
+      )
+    ]),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(20),
+      image: DecorationImage(
+          image: AssetImage('assets/images/pollock.jpg'), fit: BoxFit.cover),
+      boxShadow: [
+        BoxShadow(
+            color: Colors.black,
+            spreadRadius: 1,
+            blurRadius: 10,
+            offset: Offset(0, 1))
+      ],
+    ),
+  );
+}
+
+Widget patagoniaHilton() {
+  return Padding(
+      padding: EdgeInsets.only(top: 30),
+      child: Container(
+        height: 150,
+        child: Stack(children: [
+          Positioned(
+              top: 120,
+              left: 10,
+              child: Text(
+                "Patagonia Hilton",
+                style: TextStyle(
+                  shadows: [
+                    Shadow(
+                        blurRadius: 10,
+                        color: Colors.black,
+                        offset: Offset(1, 4))
+                  ],
+                  color: Colors.white54,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Container(
+            width: double.infinity,
+            height: 150,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20), color: Colors.black12),
+          )
+        ]),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(20),
+          image: DecorationImage(
+              image: AssetImage('assets/images/patagonia_hilton.jpg'),
+              fit: BoxFit.cover),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black,
+                spreadRadius: 1,
+                blurRadius: 10,
+                offset: Offset(0, 1))
+          ],
+        ),
+      ));
 }
